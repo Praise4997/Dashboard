029a3b125b223a00a98de6e9edf74acb56c969f7
praise adedoyin <adedoyinpraise408@gmail.com>
2022-09-30 14:07:15 +0100

    Updating the webpage

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Chair',
+        productNunmber: '85671',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Kitchen Utensils',
+        productNunmber: '23124',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Breakable plates',
+        productNunmber: '65432',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Nike Sneakers',
+        productNunmber: '85674',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Human Hair',
+        productNunmber: '56321',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
